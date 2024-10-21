b198eb50fa371dfff72a01f15f343b98fb85b44a
soyelsimo <pepebsound@gmail.com>
2024-10-22 00:39:49 +0200

    Inicio de desarrollo de clase que interactua con Gemini (LLM) para que el modelo seleccione los numeros. Clase definida, carga de la API_Key y teste de interacción realizado. Por definir el proceo completo y como conseguir que las interacciones tengan contexto de los numeros ya usados.

--- agente/algoritmos.py
+++ agente/algoritmos.py
@@ -1,5 +1,6 @@
-
-
+from langchain_google_genai import ChatGoogleGenerativeAI
+import yaml
+import os
 
 class mitades():
     def __init__(self, limite_max_rango:int) -> None:
@@ -16,20 +17,39 @@ class mitades():
         elif indicacion == "El número secreto es menor.\n":
             self.opciones = self.opciones[:len(self.opciones)//2]
 
-class llm():
-    def __init__(self) -> None:
-        pass
+class gemini_sin_memoria():
+    def __init__(self,
+                 model:str="gemini-1.5-flash",
+                 limite_max_rango = 0
+                 ) -> None:
+        
+        self.model = model
+        self.limite_max_rango = limite_max_rango
+        # Cargar API key a variable de entorno
+        with open("agente/env.yml", "r") as file:
+            os.environ["GOOGGLE_API_KEY"] = yaml.safe_load(file)["GOOGLE_API_KEY"]
+        print()
+        print("API:", os.environ["GOOGGLE_API_KEY"])
+
+        self.llm = ChatGoogleGenerativeAI(
+            model=self.model,
+            api_key=os.environ["GOOGGLE_API_KEY"])
+
+    def enviar_mensaje(self, mensaje:str):
+        mensaje = [
+            (
+                "system",
+                f"Estamos jugando que adivines un numero secreto entre el 1 y el {self.limite_max_rango}"
+            ),
+            (
+                "human",
+                mensaje
+            )
+        ]
+        respuesta = self.llm.invoke(mensaje)
+        return respuesta
 
 if __name__ == "__main__":
-    agente = mitades(limite_max_rango=1000)
-    num_secreto = 850
-    while True:
-        print(agente.seleccionar_numero())
-        if agente.ultma_seleccion < num_secreto:
-            agente.eliminar_opciones("El número secreto es mayor.\n")
-        elif agente.ultma_seleccion > num_secreto:
-            agente.eliminar_opciones("El número secreto es menor.\n")
-        else:
-            print("Has adivinado el número!!!")
-            break
-        print(f"opcion min: {agente.opciones[0]} - opcion max: {agente.opciones[-1]}")
+    agente = gemini_sin_memoria()
+    print(agente.enviar_mensaje("Recuerda esto: Tanto monta, monta tanto, Isabel como Fernando."))
+    print(agente.enviar_mensaje("¿Que te he peido que recuerdes?"))
